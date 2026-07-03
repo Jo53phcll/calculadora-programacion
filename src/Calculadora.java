12bd0c385ff6af9f75f148e14502b2bd4b79949d
Jo53phcll <jfcollaguazo@puce.edu.ec>
2026-07-03 18:54:14 -0500

    primer avance del proyecto

---
+++ src/Calculadora.java
@@ -0,0 +1,17 @@
+void main() {
+
+    String opcion = "0";
+
+    while (!opcion.equals("5")) {
+
+
+        IO.println("CALCULADORA");
+        IO.println("1. Sumar");
+        IO.println("2. Restar");
+        IO.println("3. Multiplicar");
+        IO.println("4. Dividir");
+        IO.println("5. Salir");
+
+        opcion = IO.readln("Seleccione una opción: ");
+
+ }
