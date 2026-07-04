3d4a5e885e59a1e7a38e2880f630be67cdd2b788
Jo53phcll <jfcollaguazo@puce.edu.ec>
2026-07-03 19:16:56 -0500

    actualizacion del proyecto hasta la resta

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -13,5 +13,27 @@ void main() {
         IO.println("5. Salir");
 
         opcion = IO.readln("Seleccione una opción: ");
+        switch (opcion) {
 
- }
+            case "1": {
+                int num1 = Integer.parseInt(IO.readln("Ingrese el primer número: "));
+                int num2 = Integer.parseInt(IO.readln("Ingrese el segundo número: "));
+
+                int resultado = num1 + num2;
+
+                IO.println("Resultado: " + resultado);
+                break;
+            }
+
+            case "2": {
+                int num1 = Integer.parseInt(IO.readln("Ingrese el primer número: "));
+                int num2 = Integer.parseInt(IO.readln("Ingrese el segundo número: "));
+
+                int resultado = num1 - num2;
+
+                IO.println("Resultado: " + resultado);
+                break;
+            }
+
+
+        }
